bdee8b95429a6ac96cb0577e7eddbd25b764ebfc
Wes Mason <wes@1stvamp.org>
2013-08-11 11:56:05 +0100

    Fix stupid pseudo-django model crap in signup

--- mirrit/web/models.py
+++ mirrit/web/models.py
@@ -1,4 +1,11 @@
+from bson.objectid import ObjectId
 from humbledb import Mongo, Document
+
+
+class ClassProperty (property):
+    """Subclass property to make classmethod properties possible"""
+    def __get__(self, cls, owner):
+        return self.fget.__get__(None, owner)()
 
 
 class User(Document):
@@ -25,6 +32,16 @@ class User(Document):
     def persist(self):
         with Mongo:
             if self._id:
-                super(User, self).__self_class__.update({'_id': self._id}, self, w=1)
+                super(User, self).__self_class__.update(
+                        {'_id': self._id}, self, w=1)
             else:
                 super(User, self).__self_class__.insert(self, w=1)
+
+
+class Wrapper(object):
+    def get(self, id):
+        with Mongo:
+            return User.find({'_id': ObjectId(id)})
+
+wrapper = Wrapper()
+User.query = wrapper
