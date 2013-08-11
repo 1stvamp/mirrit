ef7a40660ddb6bdb474b1b0e1aee587800a00bce
Wes Mason <wes@1stvamp.org>
2013-08-11 12:02:10 +0100

    Add github auth callback and config json handling

--- mirrit/web/app.py
+++ mirrit/web/app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+from flask.json import loads
 from flaskext import simpleregistration
+from flaskext.github import GithubAuth
 
 from mirrit.web.models import User
 from mirrit.web.forms import LoginForm, SignupForm
@@ -23,10 +26,43 @@ simplereg = simpleregistration.SimpleRegistration(
     signup_redirect="home"
 )
 
+# setup flask-github
+with open(os.path.join(os.path.dirname(os.path.dirname(
+    os.path.dirname(__file__))), 'config.json'), 'r') as f:
+
+    config_data = f.read()
+
+config = loads(config_data)
+github = GithubAuth(
+    client_id=config['client_id'],
+    client_secret=config['client_secret'],
+    session_key='user_id'
+)
+
 
 @app.route('/')
 def home():
     return 'OK', 200
+
+
+@app.route('/oauth/callback')
+@github.authorized_handler
+def authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        return redirect(next_url)
+
+    token = resp['access_token']
+    user = User.query.filter_by(github_access_token=token).first()
+    if user is None:
+        user = User(token)
+        db_session.add(user)
+    user.github_access_token = token
+    db_session.commit()
+
+    session['user_id'] = user.id
+
+    return 'Success'
 
 if __name__ == '__main__':
     # Probably never used, run with the runserver entrypoint instead
