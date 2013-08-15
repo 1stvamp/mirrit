97afc32e8ef05c3c285ad7969f95a5998286a15a
Wes Mason <wes@1stvamp.org>
2013-08-15 13:08:07 +0100

    Add twitter bootstrap

--- mirrit/web/__init__.py
+++ mirrit/web/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask('mirrit.web')
+app = Flask(__name__)
+# Add twitter-bootstrap goodness
+Bootstrap(app)
+
 app.secret_key = 'foobar'
+app.config['BOOTSTRAP_USE_MINIFIED'] = True
+app.config['BOOTSTRAP_USE_CDN'] = True
+app.config['BOOTSTRAP_FONTAWESOME'] = True
