d0f4cc1956075f3b93ed62899ac9a282f331c89e
Wes Mason <wes@1stvamp.org>
2013-08-16 01:41:31 +0100

    Add TrackedRepo model

--- mirrit/web/models.py
+++ mirrit/web/models.py
@@ -47,6 +47,15 @@ class User(db.Model):
         return bcrypt.check_password_hash(self._password, password)
 
 
+class TrackedRepo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.Enum('github', 'git', 'bitbucket'), index=True)
+    path = db.Column(db.String(256), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('users',
+                                                      lazy='dynamic'))
+
+
 def main():
     print('Creating database elements with URI "{0}"..'.format(
             app.config['SQLALCHEMY_DATABASE_URI']), file=stdout)
