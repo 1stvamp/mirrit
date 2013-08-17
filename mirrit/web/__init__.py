ce41659505037d98dadbeb6bdfece8c7b6242e9c
Wes Mason <wes@1stvamp.org>
2013-08-18 00:37:45 +0100

    Use MongoDB just for sessions

--- mirrit/web/__init__.py
+++ mirrit/web/__init__.py
@@ -4,6 +4,8 @@
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask.ext.pymongo import PyMongo
+from flask.ext.mongo_sessions import MongoDBSessionInterface
 
 app = Flask(__name__)
 # Add twitter-bootstrap goodness
@@ -14,5 +16,10 @@ app.config['BOOTSTRAP_USE_MINIFIED'] = True
 app.config['BOOTSTRAP_USE_CDN'] = True
 app.config['BOOTSTRAP_FONTAWESOME'] = True
 app.config['BOOTSTRAP_CUSTOM_CSS'] = True
+app.config['MONGO_DBNAME'] = 'mirrit'
+
+mongo = PyMongo(app)
+with app.app_context():
+    app.session_interface = MongoDBSessionInterface(app, mongo.db, 'sessions')
 
 import mirrit.web.views
